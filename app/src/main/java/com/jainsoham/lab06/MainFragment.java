2365650f32ef19b0c604a98421567fccb02e522e
2025sjain <93287597+2025sjain@users.noreply.github.com>
2024-04-29 11:28:57 -0400

    Embellishment

--- app/src/main/java/com/jainsoham/lab06/MainFragment.java
+++ app/src/main/java/com/jainsoham/lab06/MainFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -18,6 +19,7 @@ import com.google.android.material.tabs.TabLayoutMediator;
 public class MainFragment extends Fragment {
     Button mButton;
     ViewPager2 viewPager2;
+    ImageView imageView;
 
     public static MainFragment newInstance(ViewPager2 mViewPager) {
         MainFragment fragment = new MainFragment();
@@ -35,8 +37,32 @@ public class MainFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         TabLayout tabLayout = getActivity().findViewById(R.id.tab_layout);
+        imageView = view.findViewById(R.id.imageview);
         new TabLayoutMediator(tabLayout, viewPager2,
                 (tab, position)->tab.setText("I am " + (position + 1))).attach();
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                switch (position) {
+                    case 0:
+                        imageView.setImageResource(R.drawable.dog1);
+                        break;
+                    case 1:
+                        imageView.setImageResource(R.drawable.dog2);
+                        break;
+                    case 2:
+                        imageView.setImageResource(R.drawable.dog3);
+                        break;
+                    case 3:
+                        imageView.setImageResource(R.drawable.dog4);
+                        break;
+                    default:
+                        imageView.setImageResource(R.drawable.dog5);
+                        break;
+                }
+            }
+        });
         mButton = view.findViewById(R.id.button_pressme);
         mButton.setText("Press" + viewPager2.getCurrentItem());
         mButton.setOnClickListener(new View.OnClickListener() {
